9cf6e42bd3b4a6ad02d7e4bbecd5a24e9b7c366a
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-02-20 20:53:37 +0100

    reducido timeouts para que tarde menos en petar

--- deploy-scripts/beforeinstall.sh
+++ deploy-scripts/beforeinstall.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # beforeinstall.sh
 
-echo "Ejecutando tareas previas a la instalación..."
 
-# Respaldar el archivo WAR actual (si existe)
+
 if [ -f /opt/tomcat/webapps/studentify.war ]; then
-    echo "Respaldando el WAR existente..."
     cp /opt/tomcat/webapps/studentify.war /opt/tomcat/webapps/studentify.war.bak
 fi
 
-# Eliminar el directorio de la aplicación desplegada (si existe)
+
 if [ -d /opt/tomcat/webapps/studentify ]; then
-    echo "Eliminando el directorio de la aplicación antigua..."
     rm -rf /opt/tomcat/webapps/studentify
 fi
 
-echo "Tareas completadas."
+
