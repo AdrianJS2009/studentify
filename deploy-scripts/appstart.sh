2d9bc2da56e5da1468e9313846e8ee26731212d6
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-02-20 20:14:17 +0100

    update para hacerlo funcionar sin mysql y scripts de codeagent

---
+++ deploy-scripts/appstart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Start the application
+echo "Starting the application"
+systemctl start tomcat
---
+++ deploy-scripts/appstop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Stop the application
+echo "Stopping the application"
+systemctl stop tomcat
