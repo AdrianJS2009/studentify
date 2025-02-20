3d38c6b0437049102d31096afdbbef51a90f7241
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-02-20 20:57:28 +0100

    update el beforeinstall

--- deploy-scripts/beforeinstall.sh
+++ deploy-scripts/beforeinstall.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# beforeinstall.sh
 
 
 
-if [ -f /opt/tomcat/webapps/studentify.war ]; then
-    cp /opt/tomcat/webapps/studentify.war /opt/tomcat/webapps/studentify.war.bak
-fi
+cd /opt/studentify
 
 
-if [ -d /opt/tomcat/webapps/studentify ]; then
-    rm -rf /opt/tomcat/webapps/studentify
-fi
+chmod +x gradlew
 
 
+./gradlew build
+
+
+cp build/libs/studentify-1.0.0.war /opt/tomcat/webapps/studentify.war
+
+
+chown tomcat:tomcat /opt/tomcat/webapps/studentify.war
+
+echo "Tareas previas completadas."
